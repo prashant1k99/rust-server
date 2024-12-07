c53a9202b3007173987caea360cb80032ade6b9f
Prashant <prashantco111@gmail.com>
2024-12-07 16:21:53 +0530

    Base implementation of thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rust_server::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -9,17 +10,24 @@ use std::{
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("connection established");
-        handle_connection(stream);
+
+        // To setup multi-threaded server:
+        // This is not optimal as we are spinning new thread every time a request comes in.
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
+
+        // To solve that issue we will create a thread pool to handle requests
+        pool.execute(|| handle_connection(stream));
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-
-    println!("Responding");
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
