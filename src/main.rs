8535b5f05fd8cd21e60230e28f893d65df3837fc
Prashant <prashantco111@gmail.com>
2024-12-07 14:50:21 +0530

    Added 404 handling the invalid request

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,20 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    println!("Responding");
 
-    println!("Request: {http_request:#?}");
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // This returns the Ok to the server
-    // For more information about the structure of the HTTP
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
-    // stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "html/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+    };
 
-    // Let's return Hello page on request
-    let status_line = "HTTP/1.1 OK";
-    let content = fs::read_to_string("html/hello.html").unwrap();
-    let content_length = content.len();
+    let contents = fs::read_to_string(filename).unwrap();
+    let content_len = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{content}");
+    let response = format!("{status_line}\r\nContent-Length: {content_len}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
