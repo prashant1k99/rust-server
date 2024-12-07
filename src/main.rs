09021223ecce65a3655d8f69bc48fffd5ef27098
Prashant <prashantco111@gmail.com>
2024-12-07 12:16:34 +0530

    Responding with hello world
    Here we are setting our server to respond with hello world, without any
    check, simply responding Hello World html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("connection established");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    // This returns the Ok to the server
+    // For more information about the structure of the HTTP
+    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // stream.write_all(response.as_bytes()).unwrap();
+
+    // Let's return Hello page on request
+    let status_line = "HTTP/1.1 OK";
+    let content = fs::read_to_string("html/hello.html").unwrap();
+    let content_length = content.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
